31e605b1fd7b2b900aebc67a753c0c5045b42c9e
Morne Du Preez <MDuPreez2@africanbank.co.za>
2018-07-12 12:48:50 +0200

    Login Logout pages created and working

--- authapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logged_out/$', views.logged_out, name='logged_out'),
-]
