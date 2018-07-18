b97b1cbd3367d57875ce5145aa4b3d48afc5686f
Morne Du Preez <MDuPreez2@africanbank.co.za>
2018-07-18 19:48:48 +0200

    Change password option added into user profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout,  {'next_page': '/'}, name='logout'),
+    url(r'^logout/$', auth_views.logout,  {'next_page': ''}, name='logout'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^changepassword/$', views.change_password, name='change_password'),
     url(r'^profile/$', views.view_profile, name='view_profile'),
-    url(r'^profile/edit/$', views.edit_profile, name='edit_profile')
+    url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm, ProfileEditForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 def register(request):
@@ -23,8 +25,23 @@ def edit_profile(request):
 
         if form.is_valid():
             form.save()
-            return redirect('/accounts/profile')
+            return redirect('view_profile')
     else:
         form = ProfileEditForm(instance=request.user)
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
+
+def change_password(request):
+    if request.method == 'POST':
+        form=PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, user=form.user)
+            return redirect('view_profile')
+        else:
+            return redirect('change_password')
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
